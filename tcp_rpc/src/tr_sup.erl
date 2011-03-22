abdb520f6b4576422324e1f03112fe36ff4fdc1d
Simon <simon.unge.1641@student.uu.se>
2011-03-22 22:19:24 +0100

    First Commit

---
+++ tcp_rpc/src/tr_sup.erl
@@ -0,0 +1,61 @@
+%%%-------------------------------------------------------------------
+%%% @author Simon Unge <simonunge@Simon-Unges-MacBook-Air.local>
+%%% @copyright (C) 2011, Simon Unge
+%%% @doc
+%%%
+%%% @end
+%%% Created : 22 Mar 2011 by Simon Unge <simonunge@Simon-Unges-MacBook-Air.local>
+%%%-------------------------------------------------------------------
+-module(tr_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%%
+%% @spec init(Args) -> {ok, {SupFlags, [ChildSpec]}} |
+%%                     ignore |
+%%                     {error, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    Server = {tr_server, {tr_server, start_link, []},
+	               permanent, 2000, worker, [tr_server]},
+    Children = [Server],
+    RestartStrategy = {one_for_one, 0, 1},
+    {ok, {RestartStrategy, Children}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
